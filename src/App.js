f725ff5050270fce9c4a7201961a1a4fd7729d51
samchillcott1 <samchillcott@gmail.com>
2023-05-06 11:36:06 +0100

    style app nav and logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Pages from './pages/Pages';
+import Search from './components/Search';
 
 function App() {
   return (
@@ -15,5 +18,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App;
