143b273fb47c54add49830ded7996ef3f35d2d29
samchillcott1 <samchillcott@gmail.com>
2023-05-06 12:29:00 +0100

    add routes to Pages

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import { Route, Routes } from 'react-router-dom'
+
+import Home from './Home'
+import Searched from './Searched'
+import Pokemon from './Pokemon'
+
 const Pages = () => {
   return (
-    <div>Pages</div>
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+      <Route path='/searched/:search' element={ <Searched /> } />
+      <Route path='/pokemon/:name' element={ <Pokemon /> } />
+    </Routes>
   )
 }
 
