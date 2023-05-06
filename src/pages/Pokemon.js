9a348daef3eb1d24bb826ecfde919022e8896202
samchillcott1 <samchillcott@gmail.com>
2023-05-06 14:31:04 +0100

    add Pokemon component

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Pokemon = () => {
+  let params = useParams()
+  console.log("🚀 ~ file: Pokemon.js:7 ~ Pokemon ~ params:", params.name)
+  const [pokemon, setPokemon] = useState({})
+
+  const fetchDetails = async () => {
+    console.log("fetch called");
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+    const detailData = await data.json()
+    console.log("🚀 ~ file: Pokemon.js:12 ~ fetchDetails ~ detailData:", detailData)
+    setPokemon(detailData)
+  }
+
+  useEffect(() => {
+    fetchDetails()
+  }, [params.name])
+
   return (
-    <div>Pokemon</div>
+    <DetailWrapper>
+      <div className="">
+        <h2>{ pokemon.name }</h2>
+        <p>{ pokemon.weight }</p>
+        <p>{ pokemon.height }</p>
+        {/* <p>{ pokemon.abilities[0].ability.name }</p> */}
+        { pokemon.image && <img src={ pokemon.image } alt="" /> }
+        {/* <img src={ pokemon.sprites.front_default } alt="" /> */}
+      </div>
+      {/* <Info >
+        <Button
+          className={ activeTab === "instructions" ? "active" : "" }
+          onClick={ () => setActiveTab("instructions") }
+
+        >
+          Instructions
+        </Button>
+        <Button
+          className={ activeTab === "ingredients" ? "active" : "" }
+          onClick={ () => setActiveTab("ingredients") }
+        >
+          Ingredients
+        </Button>
+        { activeTab === "instructions" && (
+          <div>
+            <h3 dangerouslySetInnerHTML={ { __html: details.summary } }></h3>
+            <h3 dangerouslySetInnerHTML={ { __html: details.instructions } }></h3>
+          </div>
+        ) }
+        { activeTab === "ingredients" && (
+          <ul>
+            { details.extendedIngredients.map((ingredient) => (
+              <li key={ ingredient.id }>{ ingredient.original }</li>
+            )) }
+          </ul>
+        ) }
+      </Info> */}
+    </DetailWrapper>
   )
 }
 
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 export default Pokemon
--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import styled from 'styled-components'
+import { Link, useParams } from 'react-router-dom'
 
 const Searched = () => {
+  const [searchedPokemon, setSearchedPokemon] = useState([])
+  let params = useParams()
+
+  const getSearched = async (name) => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query=${name}&number=4`)
+    const recipes = await data.json();
+    setSearchedPokemon(recipes.results || [])
+  }
+
+  useEffect(() => {
+    getSearched(params.search)
+  }, [params.search])
+
+
   return (
-    <div>Searched</div>
+    <Grid
+      animate={ { opacity: 1 } }
+      initial={ { opacity: 0 } }
+      exit={ { opacity: 0 } }
+      transition={ { duration: 0.5 } }
+    >
+      { searchedPokemon.map((pokemon) => {
+        return (
+          <Card key={ pokemon.id }>
+            <Link to={ "/pokemon/" + pokemon.name }>
+              {/* <img src={ item.image } alt="" /> */}
+              <h4>{ pokemon.name }</h4>
+            </Link>
+          </Card>
+        )
+      }) }
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img{
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Searched
