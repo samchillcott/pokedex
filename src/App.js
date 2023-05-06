abb4fa45cb70207f8df8daf0a4163784861fcba1
samchillcott1 <samchillcott@gmail.com>
2023-05-06 10:56:57 +0100

    setup pages and components folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Nav>
+          {/* <GiKnifeFork /> */}
+          <Logo to={ "/" }>Pokedex</Logo>
+        </Nav>
+        <Search />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
