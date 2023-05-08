d8a3877c02c75415bd5e2671f6cb9ffa29990639
samchillcott1 <samchillcott@gmail.com>
2023-05-08 20:32:51 +0100

    remove searched page

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -3,14 +3,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './Home'
-import Searched from './Searched'
 import Pokemon from './Pokemon'
 
 const Pages = () => {
   return (
     <Routes>
       <Route path='/' element={ <Home /> } />
-      <Route path='/searched/:search' element={ <Searched /> } />
       <Route path='/pokemon/:name' element={ <Pokemon /> } />
     </Routes>
   )
