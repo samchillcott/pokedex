0987bc6325e57b4570476afd4e97d214cfcb0fe5
samchillcott1 <samchillcott@gmail.com>
2023-05-13 17:21:30 +0100

    extract pokemon request to hook

---
+++ src/hooks/usePokemon.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchPokemon = async (params) => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+    return response.data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default function usePokemon(params) {
+  return useQuery({ queryKey: ['pokemon'], queryFn: fetchPokemon(params) })
+}
