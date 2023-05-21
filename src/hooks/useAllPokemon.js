2203cefb9813f0b57c36aceee7b2ab610659a97d
samchillcott1 <samchillcott@gmail.com>
2023-05-21 11:00:14 +0100

    extract and update RQ hook

---
+++ src/hooks/useAllPokemon.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchAllPokemon = async () => {
+  try {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
+    console.log(response.data.results);
+    return response.data.results
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default function useAllPokemon() {
+  return useQuery({ queryKey: ['allPokemon'], queryFn: () => fetchAllPokemon() })
+}
