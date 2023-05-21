9fa41209c1613ae6b9ff9835a3e98254cf9267d4
samchillcott1 <samchillcott@gmail.com>
2023-05-21 11:23:08 +0100

    remove console.log

--- src/hooks/useAllPokemon.js
+++ src/hooks/useAllPokemon.js
@@ -4,7 +4,6 @@ import { useQuery } from '@tanstack/react-query';
 const fetchAllPokemon = async () => {
   try {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
-    console.log(response.data.results);
     return response.data.results
   } catch (error) {
     console.log(error);
